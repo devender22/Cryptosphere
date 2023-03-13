ba30a65ccd4b8f04021e2a1090291d4529db8986
Paridhi Jain <paridhijain0201@gmail.com>
2023-03-13 19:30:03 +0530

    Brought all changes to this branch

--- frontend/src/client/pages/HomePage.js
+++ frontend/src/client/pages/HomePage.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import SearchBox from '../components/SearchBox';
+import axios from 'axios';
 
 function HomePage() {
+
+  function getData(){
+    var url='http://localhost:5000/eod-data'
+    axios.get(url)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+
   return (
     <div class="Main">
       <SearchBox />
+      <form action="/post" method="post" className="form">
+          <button type="submit">Connect</button>
+      </form>
+      <button onClick={getData} type="submit">Data</button>
     </div>
   );
 }
