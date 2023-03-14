1ad19ec2dc48a96cf1942bc55ea56b9f5e761761
Abhirami R <abhirami2262@gmail.com>
2023-03-14 18:51:56 +0530

    chart added

---
+++ frontend/src/client/charts/chart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: 'My chart'
+    },
+    series: [
+      {
+        data: [1, 2, 1, 4, 3, 6]
+      }
+    ]
+  };
+  const Charts = () => (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
+  export default Charts;
